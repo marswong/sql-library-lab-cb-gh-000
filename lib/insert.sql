1fb597a36ba7fd76d0da73b95e9b36b77472c1aa
Mars Wong <marswong618@gmail.com>
2018-02-18 11:47:11 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,4 @@ INSERT INTO series (id) VALUES (1), (2);
 INSERT INTO books (id) VALUES (1), (2), (3), (4), (5), (6);
 INSERT INTO characters (id) VALUES (1), (2), (3), (4), (5), (6), (7), (8);
 INSERT INTO subgenres (id) VALUES (1), (2);
+INSERT INTO authors (id) VALUES (1), (2);
