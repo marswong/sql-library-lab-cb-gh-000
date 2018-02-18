3a99563b026a8392e008f98548789ef20352bd5d
Mars Wong <marswong618@gmail.com>
2018-02-18 12:14:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors, subgenres, series WHERE authors.id = series.author_id AND subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
