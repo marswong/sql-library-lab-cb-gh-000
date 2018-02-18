44c7c3951a9ec0aca6a6ea1f121e2c74c5e73c36
Mars Wong <marswong618@gmail.com>
2018-02-18 12:09:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, count(species) FROM characters ORDER BY count(species) DESC LIMIT 1;"
+  "SELECT species, count(species) FROM characters GROUP BY species ORDER BY count(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
